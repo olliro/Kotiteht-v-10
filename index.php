1e6be8ae1db17acc390a1d12f27a36cbbf7801c4
olliro <78209919+olliro@users.noreply.github.com>
2021-02-07 12:39:51 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f5a433;
+            font-family: 'Lato', sans-serif;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <title> lomake </title>
+</head>
+<body>
+    <div class="main">
+        <div class="lomake">
+        </div>
+    </div>
+</body>
+</html>
