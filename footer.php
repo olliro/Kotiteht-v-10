ba4be13cee9a46c25d96119dc5831de6593a4498
olliro <78209919+olliro@users.noreply.github.com>
2021-02-07 13:25:44 +0200

    Headerin ja footerin sisältö

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f59342;
+            font-family: 'Lato', sans-serif;
+        }
+        .container {
+            width: 500px;
+            margin: 0 auto;
+            align: center;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <title> Footer </title>
+</head>
+<body>
+    <div class="main">
+        <div class="container">
+            <h1> Tämä on kotitehtävän footer.</h1>
+        </div>
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: #f59342;
+            font-family: 'Lato', sans-serif;
+        }
+        .container {
+            width: 500px;
+            margin: 0 auto;
+            align: center;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <title> Header </title>
+</head>
+<body>
+    <div class="main">
+        <div class="container">
+            <h1> Tämä on kotitehtävä 10!</h1>
+        </div>
+    </div>
+</body>
+</html>
